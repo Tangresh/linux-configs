22298cf074d82206e11b7c1346d8dfdfef0ddda5
Dominik Menzi <dmenzi@tangresh.ch>
2015-09-02 22:49:07 +0200

    Add rust configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -70,6 +70,63 @@
 ;; END MELPA
 ;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;
+;; BEGIN Company
+
+;; Enable company globally for all mode
+(global-company-mode)
+
+;; Reduce the time after which the company auto completion popup opens
+(setq company-idle-delay 0.2)
+
+;; Reduce the number of characters before company kicks in
+(setq company-minimum-prefix-length 1)
+
+;; END Company
+;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;
+;; BEGIN Rust
+
+;; Load rust-mode when you open `.rs` files
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+
+;; Set path to racer binary
+(setq racer-cmd "/usr/bin/racer")
+
+;; Set path to rust src directory
+(setq racer-rust-src-path "/usr/src/rust/src/")
+
+;; Setting up configurations when you load rust-mode
+(add-hook 'rust-mode-hook
+
+          '(lambda ()
+             ;; Enable racer
+             (racer-mode)
+
+             ;; Hook in racer with eldoc to provide documentation
+             (eldoc-mode)
+
+             ;; Use flycheck-rust in rust-mode
+             (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+
+             ;; Enable company
+             (company-mode)
+
+             ;; Use company-racer in rust mode
+             (set (make-local-variable 'company-backends) '(company-racer))
+
+             ;; Key binding to jump to method definition
+             (local-set-key (kbd "M-.") #'racer-find-definition)
+
+             ;; Key binding to auto complete and indent
+             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+             )
+          )
+
+;; END Rust
+;;;;;;;;;;;;;;
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Legacy ;;;;;;;;;;;;;;;;;;
